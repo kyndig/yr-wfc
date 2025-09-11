590d4ceeccfb960d2893b70b24f76cd2e53cfcba
Axel Holene <axel.holene@gmail.com>
2025-09-11 06:37:08 +0200

    fixed linting errors

--- src/utils/location-utils.tsx
+++ src/utils/location-utils.tsx
@@ -1,11 +1,9 @@
-import { Action, ActionPanel, showToast, Toast } from "@raycast/api";
+import { Action, ActionPanel, showToast, Toast, Icon } from "@raycast/api";
 import ForecastView from "../forecast";
 import { getWeather } from "../weather-client";
 import { TimeseriesEntry } from "../weather-client";
 import { FavoriteLocation } from "../storage";
 import { WeatherFormatters } from "./weather-formatters";
-import { OpenGraphAction } from "../components/OpenGraphAction";
-import { FavoriteToggleAction } from "../components/FavoriteToggleAction";
 
 /**
  * Location utility functions to eliminate duplication
